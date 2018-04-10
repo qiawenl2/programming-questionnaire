a9d6952cbac7940a8c018612f1eaa1208ab4b891
Pierce Edmiston <pierce.edmiston@gmail.com>
2018-04-10 16:09:54 -0500

    Add script for converting csvs to rda

---
+++ bin/rdata.R
@@ -0,0 +1,21 @@
+#!/usr/bin/env Rscript
+library(devtools)
+library(readr)
+
+qualtrics <- read_csv("data-raw/responses.csv")
+questions <- read_csv("data-raw/questions.csv")
+responses <- read_csv("data-raw/responses.csv")
+languages <- read_csv("data-raw/languages.csv")
+demographics <- read_csv("data-raw/demographics.csv")
+language_ratings <- read_csv("data-raw/language-ratings.csv")
+questionnaire <- read_csv("data-raw/questionnaire.csv")
+irq <- read_csv("data-raw/irq.csv")
+language_paradigms <- read_csv("data-raw/language-paradigms.csv")
+stack_overflow <- read_csv("data-raw/stack-overflow.csv")
+stack_overflow_ranks <- read_csv("data-raw/stack-overflow-ranks.csv")
+
+use_data(qualtrics, questions, responses,
+         languages, demographics, language_ratings,
+         questionnaire, irq, language_paradigms,
+         stack_overflow, stack_overflow_ranks,
+         overwrite = TRUE)
