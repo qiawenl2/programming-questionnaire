94967372625922a0f71a56ed79cf07913522b877
Pierce Edmiston <pierce.edmiston@gmail.com>
2018-06-18 11:22:42 -0500

    Revive factors when saving to .rda"

--- bin/rdata.R
+++ bin/rdata.R
@@ -1,6 +1,9 @@
 #!/usr/bin/env Rscript
 library(devtools)
 library(readr)
+library(dplyr)
+
+source("R/question-types.R")
 
 qualtrics <- read_csv("data-raw/responses.csv")
 questions <- read_csv("data-raw/questions.csv")
@@ -8,7 +11,12 @@ responses <- read_csv("data-raw/responses.csv")
 languages <- read_csv("data-raw/languages.csv")
 demographics <- read_csv("data-raw/demographics.csv")
 language_ratings <- read_csv("data-raw/language-ratings.csv")
-questionnaire <- read_csv("data-raw/questionnaire.csv")
+
+questionnaire <- read_csv("data-raw/questionnaire.csv") %>%
+  recode_all_agreement() %>%
+  recode_all_agreement_other() %>%
+  recode_all_multiple_choice()
+
 irq <- read_csv("data-raw/irq.csv")
 language_paradigms <- read_csv("data-raw/language-paradigms.csv")
 stack_overflow <- read_csv("data-raw/stack-overflow.csv")
